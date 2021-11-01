0348c5d6c1fea585c10c3d5c30d689c385349e7a
Alan <aeaquinorosas@hotmail.com>
2021-11-01 09:43:15 -0600

    firebaseinitialconfig

---
+++ index.js
@@ -0,0 +1,20 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-app.js";
+import { getDatabase } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-database.js";
+import { getAuth ,createUserWithEmailAndPassword } from "https://www.gstatic.com/firebasejs/9.2.0/firebase-auth.js";
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyBoPSDdPBSj7IZbIHKc4yfBTszKkfUWwxE",
+  authDomain: "banco-de-tiempo-77b4e.firebaseapp.com",
+  databaseURL: "https://banco-de-tiempo-77b4e-default-rtdb.firebaseio.com",
+  projectId: "banco-de-tiempo-77b4e",
+  storageBucket: "banco-de-tiempo-77b4e.appspot.com",
+  messagingSenderId: "399972830225",
+  appId: "1:399972830225:web:064088a21e1d0cf30eb5b0",
+  measurementId: "G-J7Y6ZZT5YJ"
+};
+
+
+const app = initializeApp(firebaseConfig);
+const auth = getAuth();
